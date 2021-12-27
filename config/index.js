cec8d895c6587006ada564153b0694ee57e9d60c
Ken Robinson <krobinson@kodehaus.com>
2021-12-27 01:53:12 -0500

    update error pages

--- config/index.js
+++ config/index.js
@@ -2,7 +2,5 @@ const dotenv = require('dotenv').config();
 
   module.exports = {
     port: process.env.PORT,
-    cookieKey: process.env.APP_COOKIE_KEY,
-    _3CommasAPIKey: process.env._3CommasAPIKey,
-    _3CommasSecret: process.env._3CommasSecret
+    cookieKey: process.env.APP_COOKIE_KEY
 };
